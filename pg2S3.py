141a5db0f134864a32812ab3186abb94684e238e
Timofeev Alexey <buzzin@mail.ru>
2019-09-15 14:41:23 +0300

    First version

---
+++ pg2S3.py
@@ -0,0 +1,19 @@
+"""
+PostgreSQL to Amazon S3
+"""
+import boto3
+import subprocess
+
+
+def createbackup():
+    command = 'pg_basebackup -h localhost -D ~/backup/';
+    with Popen([command], stdout=PIPE) as proc:
+        log.write(proc.stdout.read())
+
+
+def main():
+    createbackup()
+
+
+if __name__ == "__main__":
+    main()
