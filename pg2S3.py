ccc6231bd47ed4208bfe9de73ed8593b1f67792a
Timofeev Alexey <buzzin@mail.ru>
2019-09-15 15:52:09 +0300

    First version

--- pg2S3.py
+++ pg2S3.py
@@ -7,7 +7,7 @@ import subprocess
 
 def createbackup():
     command = '/usr/bin/pg_basebackup -X fetch --format=t -D - | bzip2 -9 > ~/backup-psql-$(date +%Y-%m-%d-%H-%M).tar.bz2';
-    with subprocess.Popen([command], stdout=subprocess.PIPE) as proc:
+    with subprocess.call(command) as proc:
         log.write(proc.stdout.read())
 
 
